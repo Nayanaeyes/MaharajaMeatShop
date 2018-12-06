78360cd047efd44297093774f2a5877fbbde11e4
nayanaeyes <nayana.rj1997@gmail.com>
2018-12-06 17:04:04 +0530

    first commit

---
+++ app/src/main/java/com/kreativedigitalsolution/kdschats/MainActivity.java
@@ -0,0 +1,102 @@
+package com.kreativedigitalsolution.kdschats;
+
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+
+    SwipeRefreshLayout swipe;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+
+        swipe = findViewById(R.id.swipe);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                WebAction();
+            }
+        });
+
+        WebAction();
+    }
+
+
+    public void WebAction() {
+
+        myWebView = findViewById(R.id.webView);
+
+        WebSettings webSettings = myWebView.getSettings();
+
+        webSettings.setDomStorageEnabled(true);
+
+        webSettings.setJavaScriptEnabled(true);
+
+        //myWebView.getSettings().setAppCacheEnabled(true);
+
+        myWebView.loadUrl("http://newchat.kdssoftwares.online");
+
+
+        myWebView.setWebViewClient(new WebViewClient() {
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+
+                myWebView.loadUrl("file:///android_asset/error.html");
+
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                // do your stuff here
+                swipe.setRefreshing(false);
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
